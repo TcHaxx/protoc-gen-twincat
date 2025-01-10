2aad6b6905b89b8213dbe6795a00189c463a77b2
densogiaichned <dsian@posteo.de>
2025-01-10 16:57:38 +0100

    chore: sketch out implementation

---
+++ src/protoc-gen-twincat/ExtensionRegistryBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Google.Protobuf;
+using static TcHaxx.Extensions.v1.TchaxxExtensionsExtensions;
+
+namespace TcHaxx.ProtocGenTc;
+internal static class ExtensionRegistryBuilder
+{
+    public static ExtensionRegistry Build()
+    {
+        var registry = new ExtensionRegistry
+        {
+            MaxStringLen,
+            AttributePackMode,
+            AttributeDisplayMode,
+            AttributeTcrpcenable,
+        };
+        return registry;
+    }
+}
