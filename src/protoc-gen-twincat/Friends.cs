7886610a531021eb432d6de1d2caf8deaf0bb2ec
densogiaichned <dsian@posteo.de>
2025-01-18 09:31:50 +0100

    tests: Add test strategy

---
+++ src/protoc-gen-twincat/Friends.cs
@@ -0,0 +1,3 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("protoc-gen-twincat.Tests")]
---
+++ tests/ExtensionsHelper/TryGetExtensionTest.cs
@@ -0,0 +1,58 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Compiler;
+using Google.Protobuf.Reflection;
+using TcHaxx.ProtocGenTc;
+using Test.Extension.Helper;
+using Test.Extensions.v1;
+using static Test.Extensions.v1.ExtensionExtensions;
+
+namespace TcHaxx.ProtocGenTcTests.ExtensionsHelper;
+
+public class TryGetExtensionTest
+{
+    private const string TEST_PROTO = "test-extensions-helper.proto";
+    private readonly FileDescriptorProto? _sut;
+
+    public TryGetExtensionTest()
+    {
+        var extensionRegistry = new ExtensionRegistry
+        {
+            AttributePackMode,
+        };
+
+        var descriptorSet = FileDescriptorSet.Parser.WithExtensionRegistry(extensionRegistry).ParseFrom(File.ReadAllBytes($"./.protobufs/{TEST_PROTO}.pb"));
+
+        var request = new CodeGeneratorRequest
+        {
+            FileToGenerate = { TEST_PROTO },
+            ProtoFile = { descriptorSet.File }
+        };
+        var codeGenRequest = CodeGeneratorRequest.Parser
+            .WithExtensionRegistry(extensionRegistry)
+            .ParseFrom(request.ToByteString());
+        _sut = codeGenRequest.ProtoFile.Single(f => f.Name.EndsWith(TEST_PROTO));
+    }
+
+    [Fact]
+    public void ShouldRetrieveCorrectExtensionValue()
+    {
+        Assert.NotNull(_sut);
+
+        var md = _sut.MessageType.Single(m => m.Name == nameof(MessageWithExtension));
+        Assert.NotNull(md);
+
+        Assert.True(md.Options.TryGetExtension(AttributePackMode, out var actualPackMode));
+        Assert.Equal(EnumPackMode.EpmFourBytesAligned, actualPackMode);
+    }
+
+    [Fact]
+    public void ShouldNotRetrieveAnyExtensionValue()
+    {
+        Assert.NotNull(_sut);
+
+        var md = _sut.MessageType.Single(m => m.Name == nameof(MessageWithoutExtension));
+        Assert.NotNull(md);
+
+        Assert.False(md.Options.TryGetExtension(AttributePackMode, out var _));
+    }
+}
