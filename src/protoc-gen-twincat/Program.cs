7e9854543070de9cc2a673d8bf3d22aafcae51ca
densogiaichned <dsian@posteo.de>
2025-01-18 12:51:15 +0100

    chore: Add TC lib

--- src/protoc-gen-twincat/Program.cs
+++ src/protoc-gen-twincat/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
+using Example;
 using Google.Protobuf;
 using Google.Protobuf.Collections;
 using Google.Protobuf.Compiler;
@@ -36,6 +37,7 @@ foreach (var file in request.ProtoFile)
         continue;
     }
 
+    ST_Example.Parser.WithDiscardUnknownFields(true).ParseFrom(file.ToByteArray());
 
     var sourceCodeInfoLocation = file.SourceCodeInfo.Location;
 
